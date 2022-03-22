705cb5757814bb757692ff6e558e8f13c39a90c7
Łukasz Mróz <lmroz95@gmail.com>
2022-03-22 22:28:50 +0100

    dalsza część

---
+++ js/script.js
@@ -0,0 +1,11 @@
+<div class="container">
+<div class="row">
+    <div class="box-image-right">
+        <h2 class="info-title">Lorem ipsum</h2>
+        <p class="info-description">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+    </div>
+    <div class="box-image-right">
+        <img src="https://i.postimg.cc/rpYfCMR8/pexels-ehsan-ahmadnejad-1371998.jpg" alt="">
+    </div>
+</div>
+</div>
